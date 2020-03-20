da09924b06aaf635a7ee734227cee3bb826f5e62
FelipeEngComp <feliperibeiroalves@gmail.com>
2020-03-19 21:27:53 -0300

    Adicionando webSecurity

---
+++ src/main/java/com/interagile/cliente/escola/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.interagile.cliente.escola.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		PasswordEncoder passEnconder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth.inMemoryAuthentication().withUser("interagile").password(passEnconder.encode("mscurriculo")).roles("USER");
+	}
+
+}

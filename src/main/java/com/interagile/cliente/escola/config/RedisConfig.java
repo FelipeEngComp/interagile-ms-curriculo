27b1226c6e2b235867bc31119f07b2e35e4e389e
FelipeEngComp <feliperibeiroalves@gmail.com>
2020-04-04 02:08:40 -0300

    NOAUTH

--- src/main/java/com/interagile/cliente/escola/config/RedisConfig.java
+++ src/main/java/com/interagile/cliente/escola/config/RedisConfig.java
@@ -31,7 +31,7 @@ public class RedisConfig {
 	    poolConfig.setBlockWhenExhausted(true);
 
 	    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("ec2-35-174-250-33.compute-1.amazonaws.com", 12969);
-
+	    redisStandaloneConfiguration.setPassword("p2537c330284fca63452d961651e9c8266133f68658dea779b4bdb94dce3c6150");
 	    JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(redisStandaloneConfiguration);
 	    
 	    return jedisConnectionFactory;
